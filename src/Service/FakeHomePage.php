3846054fd387f4d17e646231bc78ed0745c16d23
Yuriy Filonenko <filonenko.us@gmail.com>
2018-10-20 12:44:41 +0300

    Creates home page service

---
+++ src/Service/FakeHomePage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+use App\Model\HomePage;
+use Faker\Factory;
+
+/**
+ * Service provides fake data for home page.
+ *
+ * @author Yuriy Filonenko <mail@gmail.com>
+ */
+final class FakeHomePage implements HomePageServiceInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getData(): HomePage 
+    {
+        $faker = Factory::create();
+        
+        return new HomePage(
+            'News Portal',
+            $faker->words(25, true),
+            'Read News',
+            'Suggest news'
+        );
+    }
+}
---
+++ src/Service/newPHPClass.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Service;
+
+/**
+ * Description of newPHPClass
+ *
+ * @author Yuriy Filonenko <mail@gmail.com>
+ */
+class newPHPClass {
+    //put your code here
+}
